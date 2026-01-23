ab98a5424cce44cede49b17ab1e03a9728752d94
paulaowodaniela-design <paulaowodaniela@gmail.com>
2026-01-23 02:40:53 +0000

    ajustes

---
+++ src/Cancion.java
@@ -0,0 +1,12 @@
+public class Cancion {
+    private String NombreC;
+    private String Artista;
+    private String Genero;
+
+
+    public Cancion(String NombreC, String Artista, String Genero){
+        this.NombreC=NombreC;
+        this.Artista=Artista;
+        this.Genero=Genero;
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+public static void main (String[] args){
+    Cancion insCancion = new Cancion("hasta la madre","Jeison Jimenez",
+    );
+}
+    
+}
