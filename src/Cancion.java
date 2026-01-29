ed30edc8fdf952cf9df874e5f37834206efa386a
paulaowodaniela-design <paulaowodaniela@gmail.com>
2026-01-29 06:22:29 +0000

    ajustes

--- src/Cancion.java
+++ src/Cancion.java
@@ -9,6 +9,25 @@ public class Cancion {
         this.Artista=Artista;
         this.Genero=Genero;
     }
-
+    //get para cambiar el private
+    public String getNombreC(){
+        return NombreC;
+    }  
+    public String getArtista(){
+        return Artista;
+    }
+    public String getGenero(){
+        return Genero;
+    }
+    // aqui los setters para modificarlos 
+    public void setNombreC(String NombreC){
+        this.NombreC=NombreC
+    }
+    public void setArtista(String Artista){
+        this.Artista=Artista
+    }
+    public void setGenero(String Genero){
+        this.Genero=Genero
+    }
     
 }
--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ public static void main (String[] args){
     Cancion insCancion = new Cancion("hasta la madre","Jeison Jimenez","Popular"
     );
     System.out.println("cancion nueva:" + insCancion.NombreC);
-    System.out.println("Titulo" + insCancion.getTitulo());
+    System.out.println("Tit" + insCancion.getTitulo());
 
 }
     
