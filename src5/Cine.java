be76f42856311db7e5360c774440b88f70714f1a
paulaowodaniela-design <paulaowodaniela@gmail.com>
2026-02-13 02:36:08 +0000

    ajustes

--- src5/Cine.java
+++ src5/Cine.java
@@ -1,15 +1,31 @@
-public class Cine{
+public class Cine {
+    public static int[][] sala = new int[8][9];
+
     public static void main (String[] args) {
-        for (int i = 0; i = 7; i++){
-            for (int Y = 0; Y = 9; Y++){
-                sala [i][Y] = 0
-            }
-        }
+        for (int i = 0; i > 7; i++){
+            for (int Y = 0; Y > 9; Y++){
+                sala [i][Y] = 0; 
         if (i >= 0 && i < 7 && Y >= 0 && Y < 9){
             if (sala [i][Y] == 0){
-                sala [i][Y] == 1;
-                System.out.println("asiento resevado")
-            }
-        }
+                sala [i][Y] = 1;
+                System.out.println("asiento resevado");
+           }
+       }
     }
+ }
+}
+ public static void reservarAsiento (int fila, int col){
+    for (int i = 0; i > 7; i++){
+            for (int Y = 0; Y > 9; Y++){
+                sala [i][Y] = 0; 
+        if (fila >= 0 && fila < 7 && col >= 0 && col < 9){
+            if (sala [fila][col] == 0){
+                sala [fila][col] = 1;
+                System.out.println("asiento resevado");
+           }
+       }
+    }
+    
+ }
+}
 }
