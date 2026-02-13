fc5ff7eafbeaba1d0dedfdb10cfbd7088fdde4da
paulaowodaniela-design <paulaowodaniela@gmail.com>
2026-02-13 02:00:43 +0000

    creacion cine

---
+++ src5/Cine.java
@@ -0,0 +1,15 @@
+public class Cine{
+    public static void main (String[] args) {
+        for (int i = 0; i = 7; i++){
+            for (int Y = 0; Y = 9; Y++){
+                sala [i][Y] = 0
+            }
+        }
+        if (i >= 0 && i < 7 && Y >= 0 && Y < 9){
+            if (sala [i][Y] == 0){
+                sala [i][Y] == 1;
+                System.out.println("asiento resevado")
+            }
+        }
+    }
+}
